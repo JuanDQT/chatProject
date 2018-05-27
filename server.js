25c824976250cfabe7ccd88360ea65ea67c34d6a
Juan <juan@MacBook-Pro-de-Juan.local>
2018-05-27 16:42:34 +0200

    added
    - Last seen
    - Is Online

--- server.js
+++ server.js
@@ -20,31 +20,29 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-var clients = [];
 var all = new Object();
 
 io.on('connection', function (socket) {
 
     // ALguien se ha conectado
-    console.log("Bienvenido de nuevo");
-    // socketIDS.push(socket.id);
-    clients.push(socket.id);
-    socket.on('login', function (msg) {
+    //console.log("Bienvenido de nuevo");
+    //clients.push(socket.id);
 
-        var data = JSON.parse(msg);
-        all[data['clientID']] = data['socketID'];
 
-/*        console.log(msg);
-        setTimeout(function () {
-            //do what you need here
-            socket.emit('home',
-                {
-                    data: "Mensaje de bienvenida: " + msg
-                }
-            );
-        }, 1000);*/
+    socket.on('LOGIN', function (msg) {
+
+        var data = JSON.parse(JSON.stringify(msg));
+
+        // if (!(data['clientID'] in all)) {
+            console.log("[LOGIN]", JSON.stringify(msg));
+            all[data['clientID']] = data['socketID'];
+            db.query('UPDATE Users set online = 1 where code = (?)', data['clientID']);
+        // }
+
+        console.log("Total in: " + Object.keys(all).length);
 
     });
+
 
     socket.on('USER_IS_TYPING', function (msg) {
 
@@ -59,21 +57,38 @@ io.on('connection', function (socket) {
 
     socket.on('MESSAGE_TO', function (msg) {
 
-        // var data = JSON.parse("'" + msg + "'");
         var data = JSON.parse(JSON.stringify(msg));
         console.log("[MESSAGE_TO]: " + JSON.stringify(msg));
-
         var socketIDTO = all[data['to']];
-
         io.sockets.to(socketIDTO).emit("GET_SINGLE_MESSAGE", data);
 
     });
 
+    socket.on('CLIENT_SET_LAST_SEEN', function (msg) {
+
+        var data = JSON.parse(JSON.stringify(msg));
+        console.log("[CLIENT_SET_LAST_SEEN]: " + JSON.stringify(msg));
+        // Notificar a todos que el esta offline y guardarlo en bbdd
+        if (data['LAST_SEEN']) {
+            db.query('UPDATE Users set last_seen = null where code = (?)', data['FROM']);
+
+        } else {
+            db.query('UPDATE Users set last_seen = now() where code = (?)', data['FROM']);
+        }
+
+    });
 
 
     // Automatico
     socket.on('disconnect', function () {
-        console.log('user disconnected');
+
+        var clientID = Object.keys(all).find(key => all[key] === socket.id);
+        console.log('socket disconnected: ' + clientID);
+        db.query('UPDATE Users set online = 0 where code = (?)', clientID);
+        delete all[clientID];
+        console.log("Total in: " + Object.keys(all).length);
+
+        //db.query('UPDATE Users set online = 1 where code = (?)', data['clientID']);
     });
 });
 
